19fb848ecd76ad8d61d31cb063347db8bf5a19d4
VikashPalani <vikash1702palani@gmail.com>
2024-10-19 11:23:49 +0530

    Login and logout features

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,44 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Navbar from './Navbar';
 import Footer from './Footer';
+import { BASE_URL } from '../utils/constants';
+import { addUser } from '../utils/userSlice';
 
 const Body = () => {
+
+  const dispatch = useDispatch();
+  const navigate =useNavigate();
+  const userData = useSelector((state) => state.user);
+
+  //Fetch user data from the server so that it can be stored in the Redux store as soon as the application loads
+  // and used throughout the application, so that the user can be authenticated before going to any route
+  const fetchUser = async () => {
+
+    if(userData) return;
+    //Make an API call to fetch user data only if the user data is not already present in the Redux store
+    try{
+    const user = await axios.get(BASE_URL + "/profile/view",
+      {withCredentials: true});
+
+      //User data will be stored in Redux store
+      dispatch(addUser(user.data));
+    } 
+    catch(err){
+      if(err.status === 401){
+        navigate("/login")
+      }
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,8 +7,9 @@ import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
 
-  const [emailId, setEmailId] = useState('');
-  const [password, setPassword] = useState('');
+  const [emailId, setEmailId] = useState('vikash@gmail.com');
+  const [password, setPassword] = useState('Vikash@123');
+  const [error,setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -23,6 +24,7 @@ const Login = () => {
         navigate('/');
     }
     catch(err){
+      setError(err?.response?.data || "Something went wrong");
       console.log(err);
     }
   };
@@ -59,6 +61,7 @@ const Login = () => {
             </label>
           </div>
 
+          <p className='text-red-500'>{error}</p>
           <div className="card-actions justify-center m-2">
             <button 
                 className="btn btn-primary w-full text-lg"
